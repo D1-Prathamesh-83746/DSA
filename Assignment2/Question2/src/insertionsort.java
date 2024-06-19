db2cc17382c7b4c99cd79ecfbc099bbe840ad7a2
D1_Prathamesh_Lawand_83746 <lawandprathamesh11@gmail.com>
2024-06-19 21:57:39 +0530

    Ass_2_Q2

---
+++ Assignment2/Question2/src/insertionsort.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class insertionsort {
+
+	public static void insertion(int arr[]) {
+		int count=0;
+		for(int i=1;i<arr.length;i++) {
+			int key=arr[i];
+			int j=i-1;
+			while(j>=0 && arr[j]<key) {
+				arr[j+1]=arr[j];
+				j--;
+				
+			}
+			count++;
+			arr[j+1]=key;
+		}
+		System.out.println(count);
+	}
+	public static void main(String[] args) {
+		int arr[]= {14,33,15,22,11};
+		System.out.println("array before sort");
+		System.out.println(Arrays.toString(arr));
+		
+		insertion(arr);
+		
+		System.out.println("array after sort");
+		System.out.println(Arrays.toString(arr));
+
+	}
+}
